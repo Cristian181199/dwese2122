d2856dadd6c0adb33919abdeb2546eccddf90879
Cristian Quintero Garcia <cristian181199@gmail.com>
2021-10-20 10:26:00 +0200

    Mostrando datos de la base de datos en una simple tabla html

--- pruebabd/index.php
+++ pruebabd/index.php
@@ -2,12 +2,28 @@
 
 $pdo = new PDO('pgsql:host=localhost;dbname=prueba', 'prueba', 'prueba');
 
-$sent = $pdo->query
-(
+$sent = $pdo->query(
     'SELECT * 
      FROM depart'
 );
 
-foreach ($sent as $fila ) {
-    echo $fila['id'] . '|' . $fila['nombre'] . '|' . $fila['localidad'] . "\n";
-}
+//foreach ($sent as $fila) {
+//    echo $fila['id'] . '|' . $fila['nombre'] . '|' . $fila['localidad'] . "\n";
+//}
+
+?>
+
+<table>
+    <tr>
+        <th>ID</th>
+        <th>NOMBRE</th>
+        <th>LOCALIDAD</th>
+    </tr>
+    <?php foreach ($sent as $fila) : ?>
+        <tr>
+            <td> <?= $fila['id'] ?> </td>
+            <td> <?= $fila['nombre'] ?> </td>
+            <td> <?= $fila['localidad'] ?> </td>
+        </tr>
+    <?php endforeach ?>
+</table>
